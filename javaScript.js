db53da43165f9432200b3026345bf8c76ff2f8bf
ElsioAyala <elsio_racing@hotmail.com>
2022-09-16 10:47:40 -0300

    se elimino la coxion a la API de zenoradio

--- javaScript.js
+++ javaScript.js
@@ -154,9 +154,9 @@ function programTimeMs(allProgramas) {
 const url = "https://tools.zenoradio.com/api/stations/" + "afnx6011qtzuv" + "/now_playing/?rand=" + Math.random();
 
 const loadMedia = async () => {
-    const response = await fetch(url)
+    /*const response = await fetch(url)
     const data = await response.json()
-    console.log("DATA:", data)
+    console.log("DATA:", data)*/
     if (/*data.title !== "Radio Ciudad 90.5" && data.title !== "Un lugar para Todos"*/ false) {
         chargeProgram = false;
         loadAutoDj(data);
